7cd5773e09fdd44ac3d455673fa84fc0794add49
Mohit1310 <mohitdayama1234@gmail.com>
2022-12-24 18:14:17 +0530

    Books Create/Index/New

---
+++ models/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://0.0.0.0:27017/mylibrary",
+  { useNewUrlParser: true },
+  (err) => {
+    if (!err) {
+      console.log("Connection Succeded " + err);
+    } else {
+      console.log("Connection Failed " + err);
+    }
+  }
+);
+
+require("./author");
--- server.js
+++ server.js
@@ -1,25 +1,40 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
+require('./models/db')
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
 
-const indexRouter = require('./routes/index')
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to mongoose'))
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-app.use('/', indexRouter)
+// const mongoose = require("mongoose");
+// mongoose.connect(
+//   process.env.DATABASE_URL,
+//   { dbName: "my-library", useNewUrlParser: true },
+//   (err) => (err ? console.log(err) : console.log("Connected to my-library"))
+// );
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.log('Connected to mongoose'))
 
-app.listen(process.env.PORT || 3000)
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+
+app.listen(3001, () => {
+    console.log('Express started at port 3001');
+});
